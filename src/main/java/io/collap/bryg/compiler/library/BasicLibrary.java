89eab5ab5d735489ad2ce937a8153788a8eafe5a
Marco Pennekamp <marco.pennekamp96@gmail.com>
2014-08-25 01:28:57 +0200

    Remove @Override (Should have been in the last commit).

--- src/main/java/io/collap/bryg/compiler/library/BasicLibrary.java
+++ src/main/java/io/collap/bryg/compiler/library/BasicLibrary.java
@@ -18,7 +18,6 @@ public class BasicLibrary implements Library {
         return functions.get (name);
     }
 
-    @Override
     public void setFunction (String name, Function function) {
         functions.put (name, function);
     }
