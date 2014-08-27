9d4d05b393a59ff489047714bd779824935699ca
Marco Pennekamp <marco.pennekamp96@gmail.com>
2014-08-27 19:25:46 +0200

    The InvalidInputParameterException is now a RuntimeException, so it does not need to be catched.

--- src/main/java/io/collap/bryg/exception/InvalidInputParameterException.java
+++ src/main/java/io/collap/bryg/exception/InvalidInputParameterException.java
@@ -3,7 +3,7 @@ package io.collap.bryg.exception;
 /**
  * This exception is thrown when a parameter is either null or has the wrong type.
  */
-public class InvalidInputParameterException extends Exception {
+public class InvalidInputParameterException extends RuntimeException {
 
     public InvalidInputParameterException (String message) {
         super (message);
