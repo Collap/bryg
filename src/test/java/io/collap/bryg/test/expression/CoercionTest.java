db869a46e49e99a23fa28656026f1e5dcc94dbaa
Marco Pennekamp <marco.pennekamp96@gmail.com>
2014-10-06 23:02:01 +0200

    Add a test that covers coercion.

---
+++ src/test/java/io/collap/bryg/test/expression/CoercionTest.java
@@ -0,0 +1,18 @@
+package io.collap.bryg.test.expression;
+
+import io.collap.bryg.model.Model;
+import io.collap.bryg.test.TemplateTest;
+
+public class CoercionTest extends TemplateTest {
+
+    @Override
+    protected void configureModel (Model model) {
+
+    }
+
+    @Override
+    protected String getTemplateName () {
+        return "expression.Coercion";
+    }
+
+}
