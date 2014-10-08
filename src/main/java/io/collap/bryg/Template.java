bdbb238758825d14e5f7900e7b732020a5c3d6bb
Marco Pennekamp <marco.pennekamp96@gmail.com>
2014-10-08 14:45:04 +0200

    Fix typo.

--- src/main/java/io/collap/bryg/Template.java
+++ src/main/java/io/collap/bryg/Template.java
@@ -8,7 +8,7 @@ import java.io.Writer;
 /**
  * Implementations of the template interface are allowed to have fields, as a new template object must be created for
  * each call to Environment.getTemplate.
- * The same template object (with the same fields) an be used to render multiple models; This means that the user
+ * The same template object (with the same fields) can be used to render multiple models; This means that the user
  * has to ensure that a template object is reused in the correct manner.
  */
 public interface Template {
