21418c0f720ecc9ea1954746e8f3daab81574aa6
Marco Pennekamp <marco.pennekamp96@gmail.com>
2014-11-15 01:37:45 +0100

    Add external fragment call test.

---
+++ src/test/java/io/collap/bryg/test/function/ExternalFragmentCallTest.java
@@ -0,0 +1,18 @@
+package io.collap.bryg.test.function;
+
+import io.collap.bryg.model.Model;
+import io.collap.bryg.test.TemplateTest;
+
+public class ExternalFragmentCallTest extends TemplateTest {
+
+    @Override
+    protected void configureModel (Model model) {
+
+    }
+
+    @Override
+    protected String getTemplateName () {
+        return "function.ExternalFragmentCall";
+    }
+
+}
